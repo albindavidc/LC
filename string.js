137ea7f6fbaa5e85aafd20cc2b51408925634da0
Albin David C <albindavidc001@gmail.com>
2024-07-30 13:04:10 +0530

    new commit

--- string.js
+++ string.js
@@ -1,7 +1,7 @@
 //1. Two Sum
 
 let twoSum = function (nums, target){
-    for(i=0;i< nums.length;i++){
+    for(i=0;i< nums.length;i++){git 
         for(j=i+1; j < nums.length; j++){
             if(nums[i] + nums[j] == target){
                 return [i,j];
@@ -16,7 +16,6 @@ console.log(twoSum(num1, 9));
 
 let num2 = [3,2,4];
 console.log(twoSum(num2, 6));
-
 
 let num3 = [3,3];
 console.log(twoSum(num3, 6));
