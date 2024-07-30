d3ff0c48a027d5d8e33bd8594eaa08995fa14db1
Albin David C <albindavidc001@gmail.com>
2024-07-30 11:34:04 +0530

    Two Sum LCP

---
+++ string.js
@@ -0,0 +1,20 @@
+//1. Two Sum
+
+let twoSum = function (nums, target){
+    for(i=0;i< nums.length;i++){
+        for(j=i+1; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i,j];
+            }
+        }
+    }
+} 
+//eg:
+let num1 = [2,7,11,15];
+console.log(twoSum(num1, 9));
+
+let num2 = [3,2,4];
+console.log(twoSum(num2, 6));
+
+let num3 = [3,3];
+console.log(twoSum(num3, 6));
