c8686809ae881fd2093537ebb7fcc259949fb42f
Albin David C <albindavidc001@gmail.com>
2024-08-14 19:31:18 +0530

    '1662.arrayStringsAreEqual'

--- array.js
+++ array.js
@@ -21,6 +21,14 @@
 
 -----------------------------------------------------------------------------
 
+//1662. Check if two array strings are equalent
+
+var arrayStringsAreEqual = function(word1, word2) {
+    word1 = word1.join("");
+    word2 = word2.join("");
+    return word1 === word2;
+};
+
 -----------------------------------------------------------------------------
 
 //744. Find smallest letter greater than target
