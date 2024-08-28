224cf01f24b3beeb3821209b30b827336617ba93
Albin David C <albindavidc001@gmail.com>
2024-08-28 08:06:13 +0530

    'checkPerfectNumber'

--- string.js
+++ string.js
@@ -21,6 +21,20 @@
 
 -----------------------------------------------------------------------------
 
+//507. Perfect Number
+
+var checkPerfectNumber = function(num) {
+    if(num<=1) return false;
+    let sum = 1;
+
+    for(i=2;i<=num/2;i++){
+        if(num%i === 0){
+            sum += i;
+        }
+    }
+    return sum === num;
+};
+
 -----------------------------------------------------------------------------
 
 //1108. Defanging IP Address
