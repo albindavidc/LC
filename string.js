0d085c1ad67cc02bed8b194389afe65956554d22
Albin David C <albindavidc001@gmail.com>
2024-10-16 17:13:37 +0530

    '#LCC34@reverseInteger'

--- string.js
+++ string.js
@@ -7,6 +7,27 @@
 
 -----------------------------------------------------------------------------
 
+// 7. Reverse Integer
+
+var reverse = function(x) {
+    let result =0;
+    let isNegative = x < 0;
+    let maxInt = Math.pow(2, 31)-1
+    x = Math.abs(x)
+
+    while(x>0){
+        let lastDigit = x%10;
+        result = result*10 + lastDigit
+        x = Math.floor(x/10);
+
+        if(result > maxInt){
+            return 0
+        }
+    }
+
+    return isNegative ? -result : result;
+
+};
 
 -----------------------------------------------------------------------------
 
