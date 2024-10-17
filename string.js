6e98de95256e16d3f8b7c4fb81a236f327fcc19b
Albin David C <albindavidc001@gmail.com>
2024-10-17 15:51:52 +0530

    '#LCC35@FirstUniqueCharacterInAString'

--- string.js
+++ string.js
@@ -5,6 +5,23 @@
 
 -----------------------------------------------------------------------------
 
+//387. First Unique Character in a String
+
+var firstUniqChar = function(s) {
+    let result = {};
+
+    for(let i = 0; i<s.length;i++){
+        result[s[i]] = (result[s[i]] || 0) +1;
+    }
+    for(let i=0; i<s.length;i++){
+        if(result[s[i]] === 1){
+            return i
+        }
+    }
+    return -1
+};
+
+
 -----------------------------------------------------------------------------
 
 // 7. Reverse Integer
