001efdaf85ae3ef3dce553f18458558d8459d7c4
Albin David C <albindavidc001@gmail.com>
2024-10-28 16:10:25 +0530

    '#LCC43@multiplyStrings'

--- string.js
+++ string.js
@@ -1,4 +1,24 @@
 
+// 43. Multiply Strings
+var multiply = function(num1, num2) {
+    if(num1 === '0' || num2 === '0') return '0';
+    let m = num1.length;
+    let n = num2.length
+    let result = Array(m+n).fill(0);
+    for(let i =num1.length -1; i>=0; i--){
+        for(let j=num2.length -1 ; j>= 0; j--){
+            const mul = num1[i] * num2[j]
+            const sum = mul + result[i+j+1]
+
+            result[i+j+1] = sum % 10;
+            result[i+j] += Math.floor(sum/10);
+        }
+    }
+
+    let resultStr = result.join('')
+    return resultStr.startsWith('0') ? resultStr.slice(1) : resultStr;
+
+-----------------------------------------------------------------------------
 
 // 917. Reverse Only Letters
 var reverseOnlyLetters = function(s) {
