69508df506ee333feacd390d15718e1001da1630
Albin David C <albindavidc007@gmail.com>
2024-11-04 15:12:46 +0530

    '#LCC47@sortAnArray'

--- array.js
+++ array.js
@@ -1,4 +1,31 @@
 
+//912. Sort an Array
+var sortArray = function (nums) {
+    if(nums.length < 2) return nums;
+
+    let midd = Math.floor(nums.length/2);
+    let left = nums.slice(0, midd);
+    let right = nums.slice(midd);
+
+    return merge(sortArray(left), sortArray(right))
+}
+
+function merge(left, right){
+    let sortedArray = [];
+
+    while(left.length && right.length){
+        if(left[0] <= right[0]){
+            sortedArray.push(left.shift());
+        }else{
+            sortedArray.push(right.shift());
+        }
+    }
+    return [...sortedArray, ...left, ...right];
+}
+
+-----------------------------------------------------------------------------
+
+
 // 75. Sort Colors
 var sortColors = function(nums) {
     let swapped;
